bb61fc1ffbe514d9b7ce3fca5a6196ee7ad3ebd2
MuhammetKadiroglu <126719242+muhammetkoyun1220@users.noreply.github.com>
2023-05-30 21:53:46 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+			  auto: true,
+			  minSlides: 1,
+			  maxSlides: 1,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			  randomStart:true,
+			  speed:3000
+			});
+    	});
